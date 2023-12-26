d3fcc4d36dfbe072b0b1886305ca0b46058fa9dc
domeeno <dominicflocea.dev@gmail.com>
2023-12-26 19:49:09 +0200

    add grep string

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
-
+vim.keymap.set('n', '<leader>sw', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  end,
+  { desc = '[S]earch [W]ord' }
+)
