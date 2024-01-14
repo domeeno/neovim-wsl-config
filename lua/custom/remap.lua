d5897032f37ac726d4125863a94255d78d33d88a
domeeno <dominicflocea.dev@gmail.com>
2024-01-14 14:33:02 +0200

    add quick quit all

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -2,6 +2,11 @@ vim.g.mapleader = " "
 
 -- COMMANDS REMAPS
 vim.keymap.set("n", "<leader><leader>", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>q", function()
+    vim.cmd("wqa")
+  end,
+  { desc = "Save and [Q]uit all" }
+)
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle [U]ndo Tree" })
 
 
@@ -16,4 +21,4 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u", "<C-u>zz")
 
 -- paste without removing the previous buffer
-vim.keymap.set("x", "<leader>p", "\"_dP", {desc = "[p]aste and preserve current buffer"})
+vim.keymap.set("x", "<leader>p", "\"_dP", { desc = "[p]aste and preserve current buffer" })
