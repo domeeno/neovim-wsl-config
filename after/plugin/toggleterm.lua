f4ae84555a4ee5862f38a9dfc6ef4abb3d02bde8
domeeno <dominicflocea.dev@gmail.com>
2024-04-14 11:19:43 +0300

    set toggleterm to floating window

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -2,7 +2,7 @@ require('toggleterm').setup {
   size = function(term)
     return vim.o.columns * 0.3
   end,
-  direction = 'vertical'
+  direction = 'float'
 }
 
 vim.keymap.set("n", "<leader>t", vim.cmd.ToggleTerm, { desc = "[T]erminal" })
