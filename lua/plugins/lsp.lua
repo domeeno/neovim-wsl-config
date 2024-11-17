f28dfef45f1a929c1417e6e75b3af964e538c40a
domeeno <dominicflocea.dev@gmail.com>
2024-11-17 18:32:06 +0200

    fix lsp bugs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,18 +6,42 @@ return {
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } }
   },
 
+  {'VonHeikemen/lsp-zero.nvim' },
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp"
+  },
+
+  { 'windwp/nvim-autopairs' },
   -- Autocompletion
   {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     config = function()
-      local cmp = require('cmp')
+      local cmp = require 'cmp'
 
-      cmp.setup({
-        sources = {
-          { name = 'nvim_lsp' },
+      -- luasnip setup
+      local luasnip = require 'luasnip'
+      require('luasnip.loaders.from_vscode').lazy_load()
+      luasnip.config.setup {}
+
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
         },
-        mapping = cmp.mapping.preset.insert({
+        mapping = cmp.mapping.preset.insert {
           ['<C-u>'] = cmp.mapping.select_next_item(),
           ['<C-d>'] = cmp.mapping.select_prev_item(),
           ['<C-p>'] = cmp.mapping.scroll_docs(-4),
@@ -45,13 +69,12 @@ return {
               fallback()
             end
           end, { 'i', 's' }),
-        }),
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
         },
-      })
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        },
+      }
     end
   },
 
@@ -64,6 +87,7 @@ return {
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'williamboman/mason.nvim' },
       { 'williamboman/mason-lspconfig.nvim' },
+      { 'VonHeikemen/lsp-zero.nvim' },
     },
     init = function()
       -- Reserve a space in the gutter
@@ -72,6 +96,7 @@ return {
     end,
     config = function()
       local lsp_defaults = require('lspconfig').util.default_config
+      local lsp_zero = require('lsp-zero')
 
       -- Add cmp_nvim_lsp capabilities settings to lspconfig
       -- This should be executed before you configure any language server
@@ -104,10 +129,10 @@ return {
       require('mason-lspconfig').setup({
         ensure_installed = {},
         handlers = {
-          -- this first function is the "default handler"
-          -- it applies to every language server without a "custom handler"
-          function(server_name)
-            require('lspconfig')[server_name].setup({})
+          lsp_zero.default_setup,
+          lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
           end,
         }
       })
