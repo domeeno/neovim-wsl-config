62e04c9ea14da8bca01b14fbbaf1b0bc6d27f702
domeeno <dominicflocea.dev@gmail.com>
2024-12-10 22:21:08 +0200

    configured jdtls

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -126,3 +126,9 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   end,
 })
 
+vim.cmd [[
+  augroup jdtls_lsp
+    autocmd!
+    autocmd FileType java lua require('config.jdtls').setup_jdtls()
+  augroup end
+]]
